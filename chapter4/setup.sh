24047584bd90773267dd332b9235e5aca525588e
Song <ada223ada@qq.com>
2018-09-27 16:18:03 +0800

    调整配置，变成4个docker虚拟机

--- chapter4/setup.sh
+++ chapter4/setup.sh
@@ -1,15 +1,20 @@
 #/bin/sh
 
+# update yum
+sudo yum update -y
+
 # install some tools
 sudo yum install -y git vim gcc glibc-static telnet bridge-utils
 
 # install docker
 curl -fsSL get.docker.com -o get-docker.sh
-sh get-docker.sh
+
+#sh /vagrant/get-docker.sh --mirror=Aliyun	
+sh get-docker.sh --mirror=Aliyun	
 
 # start docker service
 sudo groupadd docker
 sudo usermod -aG docker vagrant
-sudo systemctl start docker
+sudo systemctl start docker.service
 
 rm -rf get-docker.sh
